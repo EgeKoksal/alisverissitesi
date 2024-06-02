7245a68cfbcdc65cdda0f2cf9cb277f4fb57b511
EgeKoksal <132518271+EgeKoksal@users.noreply.github.com>
2024-06-02 18:23:43 +0300

    anasayfa güncellemeleri eklendi

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-const metinKutusu = document.querySelector('.nav-home');
-const dropBox = document.querySelector('.drop-box');
+const metinKutusu = document.querySelector(".nav-home");
+const dropBox = document.querySelector(".drop-box");
 
-metinKutusu.addEventListener('mouseover', () => {
-  dropBox.style.display = 'block';
+metinKutusu.addEventListener("mouseover", () => {
+  dropBox.style.display = "block";
 });
 
-metinKutusu.addEventListener('mouseout', () => {
-  dropBox.style.display = 'none';
+metinKutusu.addEventListener("mouseout", () => {
+  dropBox.style.display = "none";
 });
