80c6ff822eaeed23d13a88b56898c7e7c16d5dda
EgeKoksal <132518271+EgeKoksal@users.noreply.github.com>
2024-06-09 17:22:14 +0300

    bağlantılar eklendi

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 //navbar dropbox kodu
 const metinKutusu = document.querySelector(".nav-shop");
 const dropBox = document.querySelector(".drop-box");
@@ -9,17 +10,34 @@ metinKutusu.addEventListener("mouseover", () => {
 metinKutusu.addEventListener("mouseout", () => {
   dropBox.style.display = "none";
 });
-// slider butonların çalışması
 
+
+
+//anasayfa dön butonları
+const homeBtn = document.querySelector(".nav-home");
+homeBtn.addEventListener("click", () =>{
+window.location.href = "http://127.0.0.1:5501/alisverissitesi/index.html";
+});
+
+const sorniBtn = document.querySelector(".baslik");
+sorniBtn.addEventListener("click", () =>{
+window.location.href = "http://127.0.0.1:5501/alisverissitesi/index.html";
+})
+
+
+
+// slider butonların çalışması
 const mainSlider = document.querySelectorAll(".swiper-container");
 const nextButton = document.querySelector(".next");
 const prevButton = document.querySelector(".prev");
 
 
-nextButton.addEventListener("click", () => {
+
+
+/*nextButton.addEventListener("click", () => {
   mainSlider()
 });
 
 prevButton.addEventListener("click", () => {
   mainSlider()
-});
+});*/
