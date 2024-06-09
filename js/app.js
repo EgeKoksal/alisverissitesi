3fea1f702dde3ea0bef6a9831f9485cd353a9c24
EgeKoksal <132518271+EgeKoksal@users.noreply.github.com>
2024-06-09 16:09:44 +0300

    dropicon eklendi

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const metinKutusu = document.querySelector(".nav-home");
+//navbar dropbox kodu
+const metinKutusu = document.querySelector(".nav-shop");
 const dropBox = document.querySelector(".drop-box");
 
 metinKutusu.addEventListener("mouseover", () => {
@@ -7,4 +8,18 @@ metinKutusu.addEventListener("mouseover", () => {
 
 metinKutusu.addEventListener("mouseout", () => {
   dropBox.style.display = "none";
+});
+// slider butonların çalışması
+
+const mainSlider = document.querySelectorAll(".swiper-container");
+const nextButton = document.querySelector(".next");
+const prevButton = document.querySelector(".prev");
+
+
+nextButton.addEventListener("click", () => {
+  mainSlider()
+});
+
+prevButton.addEventListener("click", () => {
+  mainSlider()
 });
