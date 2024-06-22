fee786d4b8cd5065c36c7dab17ebf74b5e1043af
EgeKoksal <132518271+EgeKoksal@users.noreply.github.com>
2024-06-23 01:26:41 +0300

    blazer örnek sayfası tamamlandı

--- js/app.js
+++ js/app.js
@@ -11,28 +11,21 @@ metinKutusu.addEventListener("mouseout", () => {
   dropBox.style.display = "none";
 });
 
-
-
 //anasayfa dön butonları
 const homeBtn = document.querySelector(".nav-home");
 homeBtn.addEventListener("click", () =>{
-window.location.href = "/alısverıssıtesı/index.html";
+window.location.href = "http://127.0.0.1:5501/alisverissitesi/index.html";
 });
 
 const sorniBtn = document.querySelector(".baslik");
 sorniBtn.addEventListener("click", () =>{
-window.location.href = "/alısverıssıtesı/index.html";
+window.location.href = "http://127.0.0.1:5501/alisverissitesi/index.html";
 })
-
-
 
 // slider butonların çalışması
 const mainSlider = document.querySelectorAll(".swiper-container");
 const nextButton = document.querySelector(".next");
 const prevButton = document.querySelector(".prev");
-
-
-
 
 /*nextButton.addEventListener("click", () => {
   mainSlider()
