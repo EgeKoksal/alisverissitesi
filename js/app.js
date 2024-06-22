082f94ca2c4482413eb0edee2b26470e6bf5b0d6
EgeKoksal <132518271+EgeKoksal@users.noreply.github.com>
2024-06-23 02:37:28 +0300

    pencereye başlandı

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 //navbar dropbox kodu
 const metinKutusu = document.querySelector(".nav-shop");
 const dropBox = document.querySelector(".drop-box");
@@ -13,14 +12,27 @@ metinKutusu.addEventListener("mouseout", () => {
 
 //anasayfa dön butonları
 const homeBtn = document.querySelector(".nav-home");
-homeBtn.addEventListener("click", () =>{
-window.location.href = "http://127.0.0.1:5501/alisverissitesi/index.html";
+homeBtn.addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5501/alisverissitesi/index.html";
 });
 
 const sorniBtn = document.querySelector(".baslik");
-sorniBtn.addEventListener("click", () =>{
-window.location.href = "http://127.0.0.1:5501/alisverissitesi/index.html";
-})
+sorniBtn.addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5501/alisverissitesi/index.html";
+});
+
+//kıyafete tıklayınca pencere açılması
+const kutular = document.querySelectorAll(".image");
+const önPencere = document.querySelector(".pencere");
+const imageHover = önPencere.querySelectorAll(".image-hover")
+
+kutular.forEach(kutu => {
+  kutu.addEventListener("click", () => {
+    önPencere.style.display = "block"; // Ön pencereyi göster
+    const resimUrl = kutu.src; // Tıklanan resmin URL'sini al
+    imageHover.forEach.src = resimUrl; // Ön penceredeki resmin URL'sini ayarla
+  });
+});
 
 // slider butonların çalışması
 const mainSlider = document.querySelectorAll(".swiper-container");
